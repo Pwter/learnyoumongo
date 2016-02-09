1b75e9cc05499fd559d534abb63ed7027500d3bf
Pwter <dakcsi@gmail.com>
2016-02-09 09:47:43 +0000

    9/9 aggregation

---
+++ 09-aggregate.js
@@ -0,0 +1,26 @@
+var mongo = require('mongodb').MongoClient
+
+var url = 'mongodb://localhost:27017/learnyoumongo'
+
+mongo.connect(url,function(err,db) {
+  if (err) throw err
+  
+  var prices = db.collection('prices')
+  
+  prices.aggregate([
+    { $match: { size: process.argv[2] } }
+  , { $group: {
+      _id: 'total'
+      , total: {
+        $avg: '$price'
+        
+     }
+    
+    }}
+  ]).toArray(function(err,results){
+      if (err) throw err
+      console.log(Number(results[0].total).toFixed(2))
+      db.close()
+  })
+    
+})
